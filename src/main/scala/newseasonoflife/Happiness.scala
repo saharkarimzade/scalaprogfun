29ee0d99be1635fc5feecef0e313db32375f0923
saharkz <karimzadeh.sa@gmail.com>
2021-11-07 18:54:32 +0330

    first section of types

--- src/main/scala/newseasonoflife/Happiness.scala
+++ src/main/scala/newseasonoflife/Happiness.scala
@@ -1,5 +1,26 @@
 package newseasonoflife
 
-class Happiness {
+object Happiness {
+  def main(args: Array[String]): Unit = {
+    val unit: Unit = ()
+    println(unit)
+
+    println("_" * 50)
+    val a: Int = 123 - 456
+    println(a)
+    println("_" * 50)
+    println(Long.MaxValue)
+    //Last code prins 9223372036854775807
+    val a1: Long =9223372036854775807l
+    println(a1)
+    val a2: Long =9223372036854775807L
+    println(a2)
+    val a3: Float =9223372036854775807f
+    println(a3)
+    val a4: Double = -0.545454
+    println(a4)
+
+  }
+
 
 }
