ef1b4a45de34dfa60d77d0ecd4d4dc9cfab41c9f
saharkz <karimzadeh.sa@gmail.com>
2021-11-19 15:26:06 +0330

    scala plans and some little changes

---
+++ docs/Scala-plans.md
@@ -0,0 +1,23 @@
+#scala plans document in markdown file
+## scala subjects to read
+- [ ] difference between mutable and immutable
+- [ ] `val` and `var` and see the difference
+- [ ] how to define function with input and output(with `def`)
+- [ ] how to define function with input and output(with `val`) and see how you could define an anonymous function(Lambda)
+- [ ] match case and its guards
+- [ ] partial function
+- [ ] difference between back quotation and single quotation and double quotation
+- [ ] tuple
+- [ ] extract values from match case and tuple 
+- [ ] How to define and difference between `class`,`object`,`case class`,`trait`,`abstract class`
+- [ ] companion object for above structure(for example class or trait)
+- [ ] scala collection like `List`, `Seq`,`Map` and use their functions like `map`, `flatmap`, `flatten`, `max`, `min`, `head`, `headOption` ,`toSeq`,`toList`,`partition`,`groupBy`
+- [ ] Wrapper data structures like `Option`, `Either` and see their methods like `getOrElse` filter `exist`, `get`, `head`, `isEmpty`  
+- [ ] Async computation with `Future`
+- [ ] underline sugar syntaxs
+- [ ] for comprehension and its guards
+- [ ] `String` class method like `mkString`, String Inter Pulaition and see or investigate what is the difference with collection
+- [ ] basic knowledge of Implicits like `ExecutionContext` and define a method with Implicit duration input
+- [ ] read about `build.sbt` and its structure
+- [ ] investigate how to define Generic variables and what is their usage
+
--- src/main/scala/newseasonoflife/Happiness.scala
+++ src/main/scala/newseasonoflife/Happiness.scala
@@ -2,6 +2,26 @@ package newseasonoflife
 
 object Happiness {
   def main(args: Array[String]): Unit = {
+
+    //scala has val,var,lazy val,type
+    //val is similar to constant in java
+    val constant = 10
+    //    it is similar to this one:
+    val cons2 = {
+      println("...")
+      1339
+      //the last thing in record returned
+    }
+    //scala is not a scripting language we don't have any neccessity for complicated things like hoisting(script languages like java script, php, python, rubby, perl(linux language), bash, powershel(windows language) and ...
+    // run line to line and dosn't need compile anywher or compile line to line(like python) then we need to declare at first  and in next line get value )
+//sth about . and *
+    println("_" * 50)
+    //lazy val is mix of val and function it means this val  bind value just when it used
+    lazy val cons3 = {
+      println("...")
+      1339
+      //the last thing in record returned
+    }
     val unit: Unit = ()
     println(unit)
 
@@ -10,17 +30,57 @@ object Happiness {
     println(a)
     println("_" * 50)
     println(Long.MaxValue)
-    //Last code prins 9223372036854775807
-    val a1: Long =9223372036854775807l
+    //Last code prints 9223372036854775807
+    val a1: Long = 9223372036854775807l
     println(a1)
-    val a2: Long =9223372036854775807L
+    val a2: Long = 9223372036854775807L
     println(a2)
-    val a3: Float =9223372036854775807f
+    val a3: Float = 9223372036854775807f
     println(a3)
     val a4: Double = -0.545454
     println(a4)
+    //    use colon for characters and use semicolon for string
+    println('s')
+    println('\n')
+    println('\u2500')
+    println("hello" + " " + "world")
+    println(
+      """hello
+        |world""".stripMargin)
 
+    println(
+      """#hello
+        #world""".stripMargin('#'))
+    println(
+      """hel
+        |lo""".stripTrailing())
+    println(
+      """hel
+        |lo""".stripLineEnd)
+    println(
+      """hel
+        |lo""".substring(2))
+    println("_" * 50)
+    println(
+      """hel
+        |lo""".stripPrefix)
+    println("_" * 50)
+    var danger: Null = null
+    println(danger)
+    val  sample: String = null
+    println("_" * 50)
+    val yes: Boolean = true
+    //val bb : Int= null
+    //because of type safety of scala last code get error from compiler
+    //we can not set null in scala exsept string and null;null type is
+    // obvious but string type reason because it is a class from java
+    println(yes)
+    println("_" * 50)
+    val no: Boolean = false
+    println(no)
+    println("_" * 50)
+    val byteexample: Byte = Byte.MinValue
+    println(byteexample)
+    println("_" * 50)
   }
-
-
 }
