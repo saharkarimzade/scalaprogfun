5070d7afac3e85cf1db921fc963bd183f32a8c6f
saharkz <karimzadeh.sa@gmail.com>
2021-11-28 03:20:12 +0330

    anonymouse func

--- src/main/scala/scalaplans/DfineFuncWays.scala
+++ src/main/scala/scalaplans/DfineFuncWays.scala
@@ -14,6 +14,23 @@ def addInt( a:Int, b:Int ) : Int = {
 val evenByVal: Int => Boolean = _ % 2 == 0
 //val means call-by-value
 
+
+//how you could define an anonymous function(Lambda)?
+//An anonymous function—also referred to as a lambda—is a block of code that’s passed as an argument to a higher-order function.
+  val ints = List(1, 2, 3)
+  val doubledInts = ints.map(_ * 2)   // List(2, 4, 6)
+  val doubledInts1 = ints.map((i: Int) => i * 2)
+  val doubledInts2 = ints.map((i) => i * 2)
+  val doubledInts3 = ints.map(i => i * 2)
+  ints.foreach((i: Int) => println(i))
+  ints.foreach(i => println(i))
+  ints.foreach(println(_))
+  ints.foreach(println)
+
+  var userDir = () => { System.getProperty("user.dir") }
+  println( userDir )
+
+
 }
 
 
