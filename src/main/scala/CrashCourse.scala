620efd603411c8b0153da21a0daac33ff7e21cab
saharkz <karimzadeh.sa@gmail.com>
2021-12-08 13:32:00 +0330

    scala crash course

---
+++ src/main/scala/CrashCourse.scala
@@ -0,0 +1,29 @@
+object CrashCourse extends App {
+  //Some scala practice based on  https://www.youtube.com/watch?v=-xRfJcwhy7A&list=PLJGDHERh23x9Y1Or4y4k2p04FEfSMWIVs
+  val i = 4
+  val result: Boolean = (i == 2)
+  val result2 = i == 2
+  val result3: Any = if (false) "good" else {
+    ()
+  }
+  //In scala we can't write (condition ? result : otherwiseResult) instead of that we should write if/else
+  val result4: String = if (false) "good" else "bad"
+  val result5 = if (i == 3) "good" else "bad"
+
+
+  def wannaEatIcecreamOrWatchMovie(icecream: Boolean, movie: Boolean): String = {
+    if (icecream && movie)
+      "Sooooo coool" else if (icecream || movie)
+      "still cool buddy"+
+      "   yeah!"
+    else
+      "boring"
+      "I am not interested in!!!"
+  }
+
+  println(wannaEatIcecreamOrWatchMovie(icecream = false, movie = true))
+  //We could join two string with plus!
+  println(wannaEatIcecreamOrWatchMovie(icecream = false, movie = false))
+//As you can see the result will be "I am not interested in!!!"(the last line)
+
+}
--- src/main/scala/scalaplans/MutabaleVSImmutable.scala
+++ src/main/scala/scalaplans/MutabaleVSImmutable.scala
@@ -1,5 +1,8 @@
 package scalaplans
 
+import scala.Console.println
+import scala.collection.mutable.ListBuffer
+
 object MutabaleVSImmutable extends App{
   //first subject answer:Immutable means unchangable, Immutable In java described as this:
 //some kind of variables like Long and Int and some other that you can change the value of it but then jvm makes a new object for them
@@ -8,12 +11,27 @@ object MutabaleVSImmutable extends App{
 // another collection prodused instead of modifing original colection!
 //it differs with val and val because you can not change val anytime and every time you change a var jvm doesnt creat new object!
 //we can check it with reference
- val fruit:List[String]=List("apple","orange","pear")
+ var fruit:List[String]=List("apple","orange","pear")
 
-  val newfruit: Object =fruit:+"klsklskdlks"
+  var newfruit: Object =fruit:+"klsklskdlks"
+
+
+//  if(fruit.equals(newfruit))
+//    println("yes")
+//  else println( "no")
 print(newfruit.asInstanceOf[Object])
+val original : List[String]=List( "1" , "2")
+
+  original==( original.map(x => if(x.equals("3")) "2" else "1"))
 
 
+  var progLang = ListBuffer("C", "C++", "Java", "Scala", "Python", "JavaScript")
+
+  println("Programming Languages: " + progLang)
+
+  println("Deleting single element")
+  progLang -= "JavaScript"
+  progLang+="JavaScript"
 
 
 
