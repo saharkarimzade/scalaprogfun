dc0621bc9002ca635d61b6b2f17ab8354df85554
saharkz <karimzadeh.sa@gmail.com>
2021-12-09 08:35:29 +0330

    scalaCrashCourse

--- src/main/scala/CrashCourse.scala
+++ src/main/scala/CrashCourse.scala
@@ -29,17 +29,19 @@ object CrashCourse extends App {
   def wannaEatIcecreamOrWatchMovieByScala3(icecream: Boolean, movie: Boolean): String =
     if (icecream && movie)
       "Sooooo coool" else if (icecream || movie)
+    //use aligned intendation instead of perantises
       "still cool buddy" +
         "  scala3  yeah!"
     else
       "boring"
       "I am not interested in!!!"
-      //next line is Optional
+    //next line is Optional
     end if
   //next line is Optional
   end wannaEatIcecreamOrWatchMovieByScala3
 
   println(wannaEatIcecreamOrWatchMovieByScala3(icecream = false, movie = true))
+
   def wannaEatIcecreamOrWatchMovieBySomeCleanCode(icecream: Boolean, movie: Boolean): String =
     val iceCreamEtedOrMovieWatchedMessage: String =
       if (icecream && movie)
@@ -51,7 +53,25 @@ object CrashCourse extends App {
         "boring"
         "I am not interested in!!!"
       end if
-      //we could use parantises and end together
+    //we could use parantises and end together
     iceCreamEtedOrMovieWatchedMessage
   end wannaEatIcecreamOrWatchMovieBySomeCleanCode
+
+  //Avoid using magic values, The numbers we use Among our code without defining variables
+  //something like a>0 is not so good, we should define something like zeroNumber:Integer
+  // It can creat some situation for changing this variable in Observing clean code
+  //todo some note about mock and translation microserrvices
+  println(1377 == 1337) //it prints false
+  println(1377 != 1337) //it prints true
+  println(1377 >= 1337) //it prints true
+  println(1377 <= 1337) //it prints true
+  println(1377 < 1337) //it prints false
+  println("test" == "test")
+  println("test" == "test") //it prints true
+  //in java it would be refrential equality,whitch means equality of references, In that case when you want to
+  //compare values you should compare them  by equal
+  println("test" eq "test")
+  println("test" ne "test")
+
+
 }
